9c658c161063d4837b610f0c71c05c425222d75d
Carlos Gonzalez <carlitrosglz96@gmail.com>
2020-11-20 20:51:21 +0100

    Initial commit

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+// our components
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './slider/slider.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CareerComponent } from './career/career.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    ProjectsComponent,
+    FooterComponent,
+    SliderComponent,
+    SkillsComponent,
+    CareerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    ProjectsComponent,
+    FooterComponent,
+    SliderComponent,
+    SkillsComponent,
+    CareerComponent
   ]
 })
 export class ComponentsModule { }
