54f1f854b59de3e8a1455bd00546f537fd8ea6d7
Carlos Gonzalez <carlitrosglz96@gmail.com>
2020-11-24 10:30:00 +0100

    Release 1.0.0 preproducción. Deployment a Heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,16 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { Versioning } from '../app/interfaces/versioning.interface';
+
 export const environment = {
   production: false
+};
+
+export const DEPLOYMENT_VERSION: Versioning = {
+  development: '0.0.1',
+  preproduction: '1.0.0',
+  production: '1.0.0'
 };
 
 /*
