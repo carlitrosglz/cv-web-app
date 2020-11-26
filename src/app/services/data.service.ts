209299c5d03aab3f4abc65a40072b85c81fc725a
Carlos Gonzalez <carlitrosglz96@gmail.com>
2020-11-26 10:59:33 +0100

    Añadido enlace a CV. Acceso desde navbar

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,6 +10,8 @@ import { ProjectsModel, CareerDataModel, SkillsModel, PersonalInfoModel } from '
 })
 export class DataService {
 
+  private CV_LINK = 'https://firebasestorage.googleapis.com/v0/b/cv-portfolio-carlos.appspot.com/o/files%2FcarlosGonzalezEspejo_curriculum2.pdf?alt=media&token=dba72187-ceb4-408e-a63b-864f45106b71';
+
   constructor(private firestore: AngularFirestore) { }
 
   public getAllProjects(): Observable<ProjectsModel[]> {
@@ -34,5 +36,9 @@ export class DataService {
     return this.firestore.collection('personalData').valueChanges()
     .pipe(map(
       (data: PersonalInfoModel[]) => data));
+  }
+
+  public getCV(): string {
+    return this.CV_LINK;
   }
 }
