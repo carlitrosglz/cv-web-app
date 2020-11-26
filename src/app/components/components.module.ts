1fae04e609212d512f0b389514f65fc6503bd225
Carlos Gonzalez <carlitrosglz96@gmail.com>
2020-11-26 11:53:01 +0100

    Enlazado formulario con Angular. Control de campos. Pendiente POST del form

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
@@ -28,6 +29,7 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     CommonModule,
     RouterModule,
+    ReactiveFormsModule,
     CarouselModule
   ],
   exports: [
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { DataService } from '../../services/data.service';
 import { PersonalInfoModel } from '../../interfaces/data.interface';
@@ -11,11 +12,33 @@ import { PersonalInfoModel } from '../../interfaces/data.interface';
 export class ContactComponent implements OnInit {
 
   personalInfo: PersonalInfoModel;
+  form: FormGroup;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+              private formBuilder: FormBuilder) {
+
+    this.createForm();
+  }
 
   ngOnInit(): void {
     this.dataService.getPersonalInfo()
     .subscribe( data => this.personalInfo = data[0]);
   }
+
+  validateField(field: string): boolean {
+    return this.form.get(field).invalid && this.form.get(field).touched;
+  }
+
+  sendMessage(): void {
+    this.form.reset();
+  }
+
+  private createForm(): void{
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(1)]],
+      email: ['', [Validators.required, Validators.email, Validators.minLength(1)]],
+      message: ['', [Validators.required]]
+    });
+  }
+
 }
