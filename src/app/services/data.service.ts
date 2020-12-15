26a94a3c471a0df4d569ab0ba07a87f0acadc5b5
Carlos Gonzalez <carlitrosglz96@gmail.com>
2020-12-15 18:19:47 +0100

    Modificado enlace CV

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,7 +10,7 @@ import { ProjectsModel, CareerDataModel, SkillsModel, PersonalInfoModel } from '
 })
 export class DataService {
 
-  private CV_LINK = 'https://firebasestorage.googleapis.com/v0/b/cv-portfolio-carlos.appspot.com/o/files%2FcarlosGonzalezEspejo_curriculum2.pdf?alt=media&token=dba72187-ceb4-408e-a63b-864f45106b71';
+  private CV_LINK = 'https://firebasestorage.googleapis.com/v0/b/cv-portfolio-carlos.appspot.com/o/files%2Fcv_carlosgonzalezespejo_ES.pdf?alt=media&token=3888c969-41ef-4314-8bec-c43d8de57f7f';
 
   constructor(private firestore: AngularFirestore) { }
 
